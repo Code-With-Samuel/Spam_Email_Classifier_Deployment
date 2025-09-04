a896f4f13978d60af66d6b77ceef38fdea8367d4
Code-With-Samuel <itsme samuelgonzalez@gmail.com>
2025-09-04 16:17:33 +0545

    Debugged

--- app.py
+++ app.py
@@ -5,11 +5,10 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 
-nltk.download('punkt_tab')
+nltk.download('punkt')
 nltk.download('stopwords')
 
 ps = PorterStemmer()
-
 
 def transform_text(text):
     text = text.lower()  # lowercasing
